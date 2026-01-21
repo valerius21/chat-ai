1cffd0819dcd9b004727f43238735da91a0828d9
Valerius Mattfeld <git@valerius.me>
2026-01-21 12:40:08 +0100

    ref: update Dockerfile to use bun, adjust environment loading, and update package-lock for openai

--- back/lib/env.ts
+++ back/lib/env.ts
@@ -2,7 +2,10 @@ import dotenv from 'dotenv'
 import { envSchema } from './schema'
 
 // load env
-dotenv.config();
+dotenv.config({
+    path: ['.env.back', '../.env.back'],
+    override: true,
+});
 
 // Load environment validables and provide type-safe config
 export const Env = envSchema.parse(process.env);
